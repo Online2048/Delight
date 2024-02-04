8a97b66f362bc5da1f368cf95ab912ef8c6256f3
shital khimsuriya <shital.khimsuriya@dynacommerce.com>
2024-02-04 11:09:59 +0530

    Changed method od dynamic static sites

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -4,11 +4,7 @@ export async function generateStaticParams() {
   const posts = await fetch(
     "https://backend-online.onrender.com/getDynamicRoutesOfUserName",
     {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}),
+      method: "GET"
     }
   ).then((res) => res.json());
 
